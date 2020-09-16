b4a170c86d4ccfe170d2382291774d6686e1d60f
Emma Walker <walkere@tcd.ie>
2020-09-16 11:19:48 +0100

    first commit

---
+++ app/src/main/java/com/example/intro_to_java/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.intro_to_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.intro_to_java.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
